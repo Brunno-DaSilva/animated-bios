e77630994abf07c84a418cbbbfcb31584e086988
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-04-04 14:39:38 -0500

    created initial files, components, assets folder

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+// import { Outlet } from 'react-router-dom'
+// import Sidebar from '../Sidebar/'
+import './index.scss'
+
+const Layout = () => {
+    return (
+        <div className="App">
+            <h1>Layout</h1>
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Sidebar = () => {
+    return (
+        <div className="App">
+            <h1>Sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar
