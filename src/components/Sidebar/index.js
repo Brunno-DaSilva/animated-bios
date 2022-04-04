c41f38822718f87751631a93f3db00c333658212
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-04-04 15:19:58 -0500

    added initial layout to the navigation

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,32 @@
 import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+
+import Logo from '../../assets/images/b-logo-png-white.png'
 
 const Sidebar = () => {
     return (
-        <div className="App">
-            <h1>Sidebar</h1>
+        <div className="nav-bar">
+            <Link className="logo" to="/">
+                <img src={Logo} alt="logo" />
+                <p className="sub-logo">Bruno DaSilva</p>
+            </Link>
+            <nav className="nav">
+                <NavLink exact="true" to="/" activeClassName="active">
+                    <FontAwesomeIcon icon={faHome} color="#e76037" />
+                </NavLink>
+                <NavLink exact="true" to="/about" activeClassName="about-link">
+                    <FontAwesomeIcon icon={faUser} color="#e76037" />
+                </NavLink>
+                <NavLink
+                    exact="true"
+                    to="/contact"
+                    activeClassName="contact-link"
+                >
+                    <FontAwesomeIcon icon={faEnvelope} color="#e76037" />
+                </NavLink>
+            </nav>
         </div>
     )
 }
